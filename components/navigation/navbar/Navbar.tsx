38a43b4498ee1916969c32757b2b7a82cedef381
Rajchal <rajchalanjal1@gmail.com>
2024-11-06 21:44:31 +0545

    design

--- components/navigation/navbar/Navbar.tsx
+++ components/navigation/navbar/Navbar.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <nav className="flex-between fixed z-50 w-full gap-5 bg-white p-6 sm:px-12">
+      navbar
+    </nav>
+  );
 };
 
 export default Navbar;
