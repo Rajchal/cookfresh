ef42715c12ff3ff91b3c90a4c70a79ec9b352a5b
Rajchal <rajchalanjal1@gmail.com>
2024-11-08 17:36:28 +0545

    mobile screen blur fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,18 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+
+      "2xl": "1536px",
+      gg: "428px",
+    },
     extend: {
       colors: {
         logo: {
