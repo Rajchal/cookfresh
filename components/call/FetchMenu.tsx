10d81e2f95b1bf08597461754ba9cd77fe11ab8c
Rajchal <rajchalanjal1@gmail.com>
2024-12-07 14:04:08 +0545

    free commit

--- components/call/FetchMenu.tsx
+++ components/call/FetchMenu.tsx
@@ -7,7 +7,6 @@ interface Meal {
 
 const FetchMenu = (query: string) => {
   const dat = searchMeal(query);
-  console.log(dat);
 };
 
 const searchMeal = async (query: string) => {
@@ -15,18 +14,18 @@ const searchMeal = async (query: string) => {
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
   );
   const data = await response.json();
-  console.log(data.meals);
+  console.log(data);
   return data.meals;
 };
-function displayResults(meals: Meal[]) {
-  return (
-    <>
-      {meals.map((meal) => (
-        <div key={meal.strMeal}>
-          <h5>{meal.strMeal}</h5>
-        </div>
-      ))}
-    </>
-  );
-}
+// function displayResults(meals: Meal[]) {
+//   return (
+//     <>
+//       {meals.map((meal) => (
+//         <div key={meal.strMeal}>
+//           <h5>{meal.strMeal}</h5>
+//         </div>
+//       ))}
+//     </>
+//   );
+// }
 export default FetchMenu;
