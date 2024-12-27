09e3a801e7ec11c11ed8ce75d04b449f0280d10d
Rajchal <rajchalanjal1@gmail.com>
2024-12-27 21:31:33 +0545

    added local search and more

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
 
 import Bottom from "@/components/navigation/bottom/Bottom";
+import Navbar from "@/components/navigation/navbar/Navbar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
+      <Navbar />
       {children}
       <Bottom />
     </main>
