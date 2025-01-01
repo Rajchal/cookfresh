e01f0e22f42dd038c9ed076907c8a59b8b5e2f39
Rajchal <rajchalanjal1@gmail.com>
2025-01-01 21:17:12 +0545

    Added plan page and made few adjustments to page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,10 @@
 import { ReactNode } from "react";
 
 import Bottom from "@/components/navigation/bottom/Bottom";
-import Navbar from "@/components/navigation/navbar/Navbar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
-      <Navbar />
       {children}
       <Bottom />
     </main>
---
+++ app/(root)/plans/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <section className="border border-t-[95px]">
+        <h1 className="text-week pt-16 text-center text-logo-100">
+          Personalize your meal subscription
+        </h1>
+        <h2 className="text-5 text-nowrap py-8 text-center">
+          Choose a plan and get 5% discount every week. The more you order, the
+          more you save!
+        </h2>
+      </section>
+      <section className="flex items-center justify-center bg-[#FBF6EB]">
+        <div className="my-24 h-[616px] w-[978px] bg-white"></div>
+      </section>
+    </>
+  );
+};
+
+export default page;
