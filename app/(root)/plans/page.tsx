ef9d6632201c245ed854668852ac71bdc28531a0
Rajchal <rajchalanjal1@gmail.com>
2025-01-02 22:38:24 +0545

    Added blur functionality in page

--- app/(root)/plans/page.tsx
+++ app/(root)/plans/page.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+"use client";
 
+import { useCallback, useState } from "react";
+
+import Navbar from "@/components/navigation/navbar/Navbar";
 import PrefOption from "@/components/preference/PrefOption";
 
-const page = () => {
+const Plans = () => {
+  const [isBlur, setIsBlur] = useState(false);
+  const handleBlur = useCallback(() => {
+    setIsBlur(!isBlur);
+  }, [isBlur]);
   const options = [
     {
       title: "Time-efficient",
@@ -37,35 +44,40 @@ const page = () => {
   ];
   return (
     <>
-      <section className="border border-t-[95px]">
-        <h1 className="text-week pt-16 text-center text-logo-100">
-          Personalize your meal subscription
-        </h1>
-        <h2 className="text-5 text-nowrap py-8 text-center">
-          Choose a plan and get 5% discount every week. The more you order, the
-          more you save!
-        </h2>
-      </section>
-      <section className="flex items-center justify-center bg-[#FBF6EB]">
-        <div className="my-24 h-[616px] w-[978px] bg-white">
-          <div>
-            <p className="text-browse text-logo-100">Choose your preference</p>
-            {options.map((option) => {
-              return (
-                <PrefOption
-                  key={option.title}
-                  title={option.title}
-                  desc={option.desc}
-                  image={option.image}
-                  imagealt={option.imagealt}
-                />
-              );
-            })}
+      <Navbar handleBlur={handleBlur} />
+      <main className={`${isBlur ? "blur" : ""}`}>
+        <section className={`border border-t-[95px]`}>
+          <h1 className="text-week pt-16 text-center text-logo-100">
+            Personalize your meal subscription
+          </h1>
+          <h2 className="text-5 text-nowrap py-8 text-center">
+            Choose a plan and get 5% discount every week. The more you order,
+            the more you save!
+          </h2>
+        </section>
+        <section className="flex items-center justify-center bg-[#FBF6EB]">
+          <div className="my-24 h-[616px] w-[978px] bg-white">
+            <div>
+              <p className="text-browse text-logo-100">
+                Choose your preference
+              </p>
+              {options.map((option) => {
+                return (
+                  <PrefOption
+                    key={option.title}
+                    title={option.title}
+                    desc={option.desc}
+                    image={option.image}
+                    imagealt={option.imagealt}
+                  />
+                );
+              })}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </main>
     </>
   );
 };
 
-export default page;
+export default Plans;
